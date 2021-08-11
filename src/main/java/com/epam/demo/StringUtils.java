4cf7f720c1a5d165d9c3ff07b0408706c94fd970
Stanly0407 <Shelestova_S_V@mail.ru>
2021-08-11 11:47:43 +0300

    String utils

---
+++ src/main/java/com/epam/demo/StringUtils.java
@@ -0,0 +1,18 @@
+package com.epam.demo;
+
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+public class StringUtils {
+
+    public static boolean isPositiveNumber(String str) {
+        double number;
+        if (NumberUtils.isNumber(str)) {
+            number = NumberUtils.createDouble(str);
+        } else {
+            return false;
+        }
+        return number >= 0;
+    }
+
+}
