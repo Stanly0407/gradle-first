310e2d6697df87e4c69adcee9e8c7211847dadbb
Stanly0407 <Shelestova_S_V@mail.ru>
2021-08-11 11:47:43 +0300

    StringUtils + javadoc + tests

--- src/main/java/com/epam/demo/StringUtils.java
+++ src/main/java/com/epam/demo/StringUtils.java
@@ -3,8 +3,23 @@ package com.epam.demo;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
+/**
+ * A class {@code StringUtils} defines the single method that checks if the entered number is positive.
+ *
+ * @author Sviatlana Shelestava
+ * @since 1.3.5
+ */
 public class StringUtils {
 
+    /**
+     * <p>Checks whether the String a positive number.</p>
+     *
+     * <p><code>Null</code> and empty String will return
+     * <code>false</code>.</p>
+     *
+     * @param str the <code>String</code> to check
+     * @return <code>true</code> if the string is a positive number
+     */
     public static boolean isPositiveNumber(String str) {
         double number;
         if (NumberUtils.isNumber(str)) {
@@ -12,7 +27,7 @@ public class StringUtils {
         } else {
             return false;
         }
-        return number >= 0;
+        return number > 0;
     }
 
 }
